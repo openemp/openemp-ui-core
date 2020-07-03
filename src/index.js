26d0e73d7dc26a427e1017b1b5f49be08ac78022
honohunter <bbbooo1994@gmail.com>
2020-07-03 16:31:59 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import './set-public-path';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
+import Root from './root.component';
+
+// eslint-disable-next-line no-undef
+
+function domElementGetter() {
+  let el = document.getElementById('root');
+  if (!el) {
+    el = document.createElement('div');
+    el.id = 'root';
+    document.body.appendChild(el);
+  }
+  return el;
+}
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Root,
+  errorBoundary() {
+    return React.createElement('div', null, 'error');
+  },
+  domElementGetter,
+});
+
+export const { bootstrap, mount, unmount } = lifecycles;
+export { links } from './root.helper';
+
+export const devtools = {
+  overlays: {
+    selectors: ['.root.dashboardHeight'],
+    options: {
+      color: 'red',
+    },
+  },
+};
---
+++ src/root.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mountRootParcel } from 'single-spa';
+import ParcelComponent from 'single-spa-react/parcel';
+import { createJss, rtl, jssPreset, StylesProvider, useTheme } from '@openemp-mf/styleguide';
+import { withAuth } from '@openemp-mf/login';
+
+// eslint-disable-next-line no-undef
+const remoteImport = async (url) => System.import(url);
+const jss = createJss({ plugins: [...jssPreset().plugins, rtl()] });
+
+const Root = () => {
+  const theme = useTheme();
+  const lang = JSON.parse(localStorage.getItem('lang'));
+  if (lang) {
+    theme.direction = lang.dir;
+    document.querySelector('body').setAttribute('dir', lang.dir);
+  }
+
+  return ['@openemp-mf/navbar', '@openemp-mf/drawer'].map((ele) => (
+    <ParcelComponent
+      key={ele}
+      config={remoteImport(ele)}
+      mountParcel={mountRootParcel}
+      StylesProvider={StylesProvider}
+      jss={jss}
+    />
+  ));
+};
+
+export default withAuth(Root);
